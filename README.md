3813690db9d469ef209578a75f722fb59ee5f0bb
Samir Vyas <vyas.samir1994@gmail.com>
2024-12-31 12:49:14 -0500

    removed venv, inserts work as long as mysql is running

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Website to keep track of my sleep apnea equipment history.
 - Connected to Render
 - Can view website once you unsuspend service - https://apnea.onrender.com
 - pip freeze > requirements.txt
-
+- .\venv\Scripts\activate
 
 # TODO
 - Think of what type of storage needed(trying json, seems complicated)
 - Build UI
 - load images in grid
+- flask --app 
--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-import json
+import json, pymysql
 from flask import Flask, request, jsonify, render_template
 
+
 app = Flask(__name__)
+config_path ='config.json'
+with open(config_path, 'r') as file:
+    config = json.load(file)
+    db_config = config.get("database", {})
 
 @app.route('/')
 def home():
     return render_template('index.html')
-    # return app.send_static_file('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    data = request.json  # Get JSON data from the request
+    
+    data = request.json  
     selected_date = data.get('selected_date')
+    item_name = data.get('item_name')
+
+    insert_data_to_db(config, data)
 
     if not selected_date:
         return jsonify({"success": False, "message": "No date provided"}), 400
@@ -23,12 +31,56 @@ def submit():
     except FileNotFoundError:
         existing_data = []
 
-    existing_data.append({"selected_date": selected_date})
+    existing_data.append({"selected_date": selected_date, "item_name":item_name})
 
     with open(json_file_path, 'w') as json_file:
         json.dump(existing_data, json_file, indent=4)
 
     return jsonify({"success": True, "message": f"Date {selected_date} saved successfully!"})
 
+def insert_data_to_db(config, data):
+      
+    selected_date = data.get('selected_date')
+    item_name = data.get('item_name')
+    
+    connection = connect_to_mysql(config)
+        
+    try:
+        query = "INSERT INTO cpap_parts (item_name, timestamp) VALUES (%s, %s)"
+        print(f"Inserting item_name: {item_name}, selected_date: {selected_date}")
+        with connection.cursor() as cursor:
+            cursor.execute(query, (item_name, selected_date))
+            connection.commit()
+            print(f"Data inserted successfully.")
+    except pymysql.MySQLError as e:
+        print(f"Error inserting data into MySQL: {e.args[0]}, {e.args[1]}")
+
+    finally:
+        if 'connection' in locals() and connection.open:
+            connection.close()
+            print("MySQL connection closed after insertion.")
+    
+
+def connect_to_mysql(config):
+    
+    try:
+        connection = pymysql.connect(
+            host=db_config.get("host"),
+            port=db_config.get("port"),
+            user=db_config.get("user"),
+            password=db_config.get("password"),
+            database=db_config.get("database")
+        )
+        print("Connected to the MySQL database successfully.")
+        
+        return connection
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to MySQL: {e}")
+    # finally:
+    #     if 'connection' in locals() and connection.open:
+    #         connection.close()
+    #         print("MySQL connection closed.")
+
 if __name__ == '__main__':
+    connect_to_mysql(config)
     app.run(debug=True)
